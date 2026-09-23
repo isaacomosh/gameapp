cb0dbda5f2ea6e8d8cf46fa7832b065103afedac
iceharc <isaacelly85@gmail.com>
2026-09-23 22:06:45 +0300

    Added home page

---
+++ gameapp/lib/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeUI extends StatefulWidget {
+  const HomeUI({super.key});
+
+  @override
+  State<HomeUI> createState() => _HomeUIState();
+}
+
+class _HomeUIState extends State<HomeUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [Text("gamefy", style: GoogleFonts.roboto())]),
+      ),
+    );
+  }
+}
