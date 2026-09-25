ba0fa434be3cd221f0e99ef082cac217964c868f
iceharc <isaacelly85@gmail.com>
2026-09-25 14:34:57 +0300

    Added category buttons and popular games cards added rating stars to game cards

--- gameapp/lib/pages/home.dart
+++ gameapp/lib/pages/home.dart
@@ -20,7 +20,7 @@ class _HomeUIState extends State<HomeUI> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 20),
+                SizedBox(height: 0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -55,7 +55,8 @@ class _HomeUIState extends State<HomeUI> {
                   "Hey, Isaac",
                   style: GoogleFonts.nunitoSans(
                     fontWeight: FontWeight.bold,
-                    color: Colors.grey,
+                    color: Colors.black,
+                    fontSize: 17,
                   ),
                 ),
                 SizedBox(height: 10),
@@ -67,113 +68,9 @@ class _HomeUIState extends State<HomeUI> {
                     fontSize: 25,
                   ),
                 ),
+
                 SizedBox(height: 10),
-                Container(
-                  width: 500,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 12,
-                        spreadRadius: 1,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Stack(
-                      children: [
-                        // Game image
-                        Positioned.fill(
-                          child: Image.asset(
-                            "assets/games/fortnite.jpg",
-                            fit: BoxFit.cover,
-                          ),
-                        ),
 
-                        // Dark overlay
-                        Positioned.fill(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [
-                                  Colors.transparent,
-                                  Colors.black.withOpacity(0.75),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        // Game name + type
-                        Positioned(
-                          left: 16,
-                          bottom: 12,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Fortnite",
-                                style: GoogleFonts.poppins(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              Text(
-                                "Battle Royale",
-                                style: GoogleFonts.poppins(
-                                  color: Colors.white70,
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        // Rating
-                        Positioned(
-                          right: 14,
-                          top: 12,
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 6,
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.65),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Row(
-                              children: [
-                                const Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                  size: 16,
-                                ),
-                                const SizedBox(width: 4),
-                                Text(
-                                  "4.8",
-                                  style: GoogleFonts.poppins(
-                                    color: Colors.white,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
                 Stack(
                   children: [
                     //1.Main game image
@@ -201,16 +98,259 @@ class _HomeUIState extends State<HomeUI> {
                     ),
 
                     Positioned(
-                      top: 12,
+                      top: 170,
                       right: 12,
                       child: Container(
                         padding: EdgeInsets.symmetric(horizontal: 10),
                         child: Row(
-                          children: [Icon(Icons.star, color: Colors.amber)],
+                          children: [
+                            Icon(Icons.star, color: Colors.amber),
+                            Icon(Icons.star, color: Colors.amber),
+                            Icon(Icons.star, color: Colors.amber),
+                            Icon(Icons.star_half, color: Colors.amber),
+
+                            SizedBox(width: 4),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    Positioned(
+                      left: 10,
+                      bottom: 16,
+                      child: Column(
+                        children: [
+                          Text(
+                            "Fortnite",
+                            style: GoogleFonts.poppins(
+                              color: Colors.white70,
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Battle Royale",
+                            style: GoogleFonts.poppins(
+                              color: Colors.white70,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Categories",
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 237, 236, 236),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sci-Fi",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 60,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 237, 236, 236),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Racing",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 60,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 237, 236, 236),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sports",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 60,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 237, 236, 236),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Royale",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
                   ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Popular Games",
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "See All",
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 14),
+                SizedBox(
+                  height: 190,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Stack(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              "assets/games/gta6.jpg",
+                              width: 190,
+                              height: 190,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+
+                          Positioned.fill(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                gradient: const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [Colors.transparent, Colors.black87],
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          Positioned(
+                            left: 10,
+                            right: 10,
+                            bottom: 10,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Grand Theft Auto",
+                                  style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+
+                                const Icon(Icons.star, color: Colors.amber),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      const SizedBox(width: 15),
+
+                      // Second card
+                      Stack(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              "assets/games/gta6.jpg",
+                              width: 190,
+                              height: 190,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+
+                          Positioned.fill(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                gradient: const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [Colors.transparent, Colors.black87],
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          Positioned(
+                            left: 10,
+                            right: 10,
+                            bottom: 10,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Grand Theft Auto",
+                                  style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+
+                                const Icon(Icons.star, color: Colors.amber),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
